cb608f2ebc2174cec36b19c3ecbaa8a2119505cb
Caique Dias de Melo <caiquedm11@gmail.com>
2026-06-18 16:14:27 -0300

    feat: Começando a criar as resources

---
+++ app/Filament/Resources/Chamados/Schemas/ChamadoForm.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources\Chamados\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Schema;
+
+class ChamadoForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Tabs::make("")
+                    ->columnSpanFull()
+                    ->schema([
+                        Tabs\Tab::make('Dados do Chamado')
+                            ->columns(2)
+                            ->schema([
+                                Group::make()
+                                    ->columnSpan(2)
+                                    ->columns(6)
+                                    ->schema([
+                                        Select::make('empresa')
+                                            ->columnSpan(2)
+                                            ->label('Empresa')
+                                            ->required()
+                                            ->options([
+                                                'royal' => 'Royal',
+                                                'tsbox' => 'TsBox',
+                                            ])
+                                            ->default('royal'),
+                                        TextInput::make('codigo_referencia')
+                                            ->label('Código de Referência')
+                                            ->columnSpan(1)
+                                            ->required(),
+                                        TextInput::make('titulo')
+                                            ->label('Título')
+                                            ->columnSpan(3)
+                                            ->required(),
+                                    ]),
+                                Select::make('participantes')
+                                    ->label('Participantes')
+                                    ->multiple()
+                                    ->preload()
+                                    ->relationship('participantes', 'name'),
+                                TextInput::make('tempo_estimado_minutos')
+                                    ->label('Tempo Estimado (minutos)')
+                                    ->numeric(),
+                                MarkdownEditor::make('descricao')
+                                    ->label('Descrição')
+                                    ->columnSpanFull(),
+                                MarkdownEditor::make('solucao')
+                                    ->label('Solução')
+                                    ->columnSpanFull(),
+                            ]),
+
+                        Tabs\Tab::make('Work logs')
+                            ->columns(2)
+                            ->schema([
+                                Repeater::make('workLogs')
+                                    ->relationship('workLogs')
+                                    ->addActionLabel('Adicionar novo work log')
+                                    ->columns(6)
+                                    ->columnSpanFull()
+                                    ->schema([
+                                        DateTimePicker::make('comeco_em')
+                                            ->label('Início')
+                                            ->required(),
+                                        DateTimePicker::make('termino_em')
+                                            ->label('Término')
+                                            ->required(),
+                                        TextInput::make('descricao')
+                                            ->label('Descrição')
+                                            ->nullable(),
+                                    ]),
+                            ]),
+                    ])
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Participantes/Schemas/ParticipanteForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\Participantes\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ParticipanteForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+            ]);
+    }
+}
